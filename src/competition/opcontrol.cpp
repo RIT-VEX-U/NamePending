5c7f6df2332999a0cea01c94e222f9344aca9dd4
PascalSkylake <jackcammarata1@gmail.com>
2025-01-31 02:51:13 -0500

    redauto

--- src/competition/opcontrol.cpp
+++ src/competition/opcontrol.cpp
@@ -19,7 +19,7 @@ int color_sensor_counter = 0;
 
 bool conveyor_started = false;
 
-bool blue_alliance = true;
+bool blue_alliance = false;
 
 AutoCommand *intake_command(double amt = 12.0) {
     return new FunctionCommand([=]() {
@@ -401,27 +401,28 @@ void testing() {
 
       // Goal Rush
       new Parallel{
-        new DriveForwardCommand(drive_sys, drive_motioncontroller, 49, vex::reverse, 1, 0),
-        new InOrder{new DelayCommand(1380), goal_grabber_command(true)}
+        drive_sys.DriveForwardCmd(49, vex::reverse, 1, 0),
+        new Async{new InOrder{new DelayCommand(970), goal_grabber_command(true)}}
       },
 
       // Reverse a bit with the goal
-      new DriveForwardCommand(drive_sys, drive_motioncontroller, 22, vex::forward, 1, 0),
+      drive_sys.DriveForwardCmd(22, vex::forward, 1, 0)->withTimeout(2),
 
       // First set of rings
-      drive_sys.TurnToPointCmd(96, 24, vex::forward, 1, 0), conveyor_intake_command(),
-      new DriveToPointCommand(drive_sys, drive_motioncontroller, {96, 24}, vex::forward, 1, 0),
+      drive_sys.TurnToPointCmd(48, 24, vex::forward, 0.8, 0)->withTimeout(0.5), 
+      conveyor_intake_command(),
+      drive_sys.DriveToPointCmd({48, 24}, vex::forward, 1, 0)->withTimeout(2),
       drive_sys.DriveForwardCmd(8, vex::forward, 0.8, 0)->withTimeout(2),
       drive_sys.DriveForwardCmd(8, vex::reverse, 0.8, 0)->withTimeout(2),
 
       // Second set of rings
-      drive_sys.TurnToHeadingCmd(0, 1, 0)->withTimeout(1),
-      drive_sys.DriveToPointCmd({120, 24}, vex::fwd, 1, 0)->withTimeout(2),
+      drive_sys.TurnToHeadingCmd(180, 0.8, 0)->withTimeout(1),
+      drive_sys.DriveToPointCmd({24, 24}, vex::fwd, 1, 0)->withTimeout(2),
       drive_sys.DriveForwardCmd(10, vex::forward, 0.8, 0)->withTimeout(2),
       drive_sys.DriveForwardCmd(10, vex::reverse, 0.8, 0)->withTimeout(2),
 
       // Corner shit
-      drive_sys.TurnToHeadingCmd(-43, 1, 0)->withTimeout(1),
+      drive_sys.TurnToHeadingCmd(-208, 0.8, 0)->withTimeout(1),
       drive_sys.DriveForwardCmd(16, vex::forward, 0.4, 0)->withTimeout(1.5),
       drive_sys.DriveForwardCmd(13, vex::reverse, 0.4, 0)->withTimeout(1.5),
       drive_sys.DriveForwardCmd(16, vex::forward, 0.4, 0)->withTimeout(1.5),
@@ -432,7 +433,7 @@ void testing() {
       drive_sys.DriveForwardCmd(13, vex::reverse, 0.4, 0)->withTimeout(1.5),
 
       // Drop goal in corner
-      drive_sys.TurnToHeadingCmd(135, 0.8, 0)->withTimeout(0.5),
+      drive_sys.TurnToHeadingCmd(45, 0.8, 0)->withTimeout(0.5),
       goal_grabber_command(false),
       drive_sys.DriveTankCmd(-0.3, -0.3)->withTimeout(0.5),
 
